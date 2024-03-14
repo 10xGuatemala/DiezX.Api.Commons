15c1bcb19ecd5991aae3e3ee50625a434115526b
guerraconnor <miguelguerra@10x.gt>
2024-03-14 16:53:41 -0600

    Cambios al copyrigth
    DiezX.Api.Commons para a 10X de Guatemala

--- Collections/CollectionUtil.cs
+++ Collections/CollectionUtil.cs
@@ -1,5 +1,5 @@
 ﻿//
-//  Copyright 2022  Soluciones Modernas 10x
+//  Copyright © 2024 10X de Guatemala, S.A.
 //
 //    Licensed under the Apache License, Version 2.0 (the "License");
 //    you may not use this file except in compliance with the License.
